3f6b55cfe28446c8e062d11ec8d91bab7e4702aa
KiwiScreams <screamskiwi3@gmail.com>
2024-08-08 21:35:12 +0400

    panel aniation

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import "./Panel.css";
 const Panel = ({ onHide, showPanel }) => {
+  const [isClosing, setIsClosing] = useState(false);
   const handleClose = () => {
-    onHide();
+    setIsClosing(true);
+    setTimeout(() => {
+      onHide();
+      setIsClosing(false);
+    }, 300);
   };
 
   return (
     <>
       {showPanel && (
-        <div className="overlay">
-          <section className="auth-panel">
+        <div
+          className="overlay"
+          onAnimationEnd={() => setIsClosing(false)}
+        >
+          <section className={`auth-panel ${isClosing ? "closing" : ""}`}>
             <button className="x" onClick={handleClose}>
               <i className="fa-solid fa-x"></i>
             </button>
