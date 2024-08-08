fb2767af7cb298528478892cd357bd4fc1206d1d
KiwiScreams <screamskiwi3@gmail.com>
2024-08-08 21:19:03 +0400

    panel

--- src/components/panel/Panel.jsx
+++ src/components/panel/Panel.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import "./Panel.css";
-const Panel = () => {
+const Panel = ({ onHide, showPanel }) => {
+  const handleClose = () => {
+    onHide();
+  };
+
   return (
     <>
-      <h1>panel</h1>
+      {showPanel && (
+        <div className="overlay">
+          <section className="auth-panel">
+            <button className="x" onClick={handleClose}>
+              <i className="fa-solid fa-x"></i>
+            </button>
+            <h3>შესვლა</h3>
+            <label htmlFor="email">ელ-ფოსტა</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Example@redberry.ge"
+            />
+            <button className="submit">შესვლა</button>
+          </section>
+        </div>
+      )}
     </>
   );
 };
--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,17 +1,32 @@
 import { NavLink } from "react-router-dom";
 import redberryLogo from "../../../assets/images/redberry.svg";
 import "./Header.css";
+import { useState } from "react";
+import Panel from "../../panel/Panel";
 const Header = () => {
-    return ( 
-        <>
-        <header>
-            <nav>
-                <NavLink to="/"><img src={redberryLogo} alt="" /></NavLink>
-                <button>შესვლა</button>
-            </nav>
-        </header>
-        </>
-     );
-}
- 
+  const [showPanel, setShowPanel] = useState(false);
+
+  const handlePanelShow = () => {
+    setShowPanel(true);
+  };
+
+  const handlePanelHide = () => {
+    setShowPanel(false);
+  };
+
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/">
+            <img src={redberryLogo} alt="" />
+          </NavLink>
+          <button onClick={handlePanelShow}>შესვლა</button>
+        </nav>
+      </header>
+      <Panel showPanel={showPanel} onHide={handlePanelHide} />{" "}
+    </>
+  );
+};
+
 export default Header;
