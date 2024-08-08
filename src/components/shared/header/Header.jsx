2db120c9b332da4e7527f175a4d4bfae1df72034
KiwiScreams <screamskiwi3@gmail.com>
2024-08-08 11:50:04 +0400

    header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,8 +1,15 @@
+import { NavLink } from "react-router-dom";
+import redberryLogo from "../../../assets/images/redberry.svg";
 import "./Header.css";
 const Header = () => {
     return ( 
         <>
-
+        <header>
+            <nav>
+                <NavLink to="/"><img src={redberryLogo} alt="" /></NavLink>
+                <button>შესვლა</button>
+            </nav>
+        </header>
         </>
      );
 }
