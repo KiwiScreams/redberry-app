9b146eec3932bdb72f9ed4ea616bb41863c1b062
KiwiScreams <screamskiwi3@gmail.com>
2024-08-12 21:24:08 +0400

    button

--- src/pages/add blog/AddBlog.jsx
+++ src/pages/add blog/AddBlog.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import "./AddBlog.css";
 const AddBlog = () => {
-    return ( 
-        <>
-        addBlogs
-        </>
-     );
-}
- 
+    const navigate = useNavigate();
+    const previewPage = () =>
+    {
+        navigate("/");
+    }
+  return (
+    <>
+      <section className="add-blog-section">
+        <button onClick={previewPage}>
+          <i class="fa-solid fa-chevron-left"></i>
+        </button>
+      </section>
+    </>
+  );
+};
+
 export default AddBlog;
