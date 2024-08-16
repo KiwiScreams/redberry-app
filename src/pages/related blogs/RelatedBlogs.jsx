3485e0184c448ff47ce8c82b62ab3fb2cdaab739
KiwiScreams <screamskiwi3@gmail.com>
2024-08-16 19:06:01 +0400

    related blogs category

--- src/pages/related blogs/RelatedBlogs.jsx
+++ src/pages/related blogs/RelatedBlogs.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import "./RelatedBlogs.css";
 import { Link } from "react-router-dom";
 import arrowImage from "../../assets/images/Arrow.svg";
+const getCategoryClassName = (category) => {
+  switch (category) {
+    case "Market":
+      return "market";
+    case "Application":
+      return "application";
+    case "AI":
+      return "ai";
+    case "UI/UX":
+      return "ui-ux";
+    case "Explore":
+      return "explore";
+    case "Figma":
+      return "figma";
+    default:
+      return "";
+  }
+};
 const RelatedBlogs = ({ relatedBlogs }) => {
   let filters = [
     { category: "Market", label: "მარკეტი" },
@@ -11,29 +29,11 @@ const RelatedBlogs = ({ relatedBlogs }) => {
     { category: "Explore", label: "კვლევა" },
     { category: "Figma", label: "Figma" },
   ];
-
   const categoryMap = filters.reduce((acc, curr) => {
     acc[curr.category] = curr.label;
     return acc;
   }, {});
-  const getCategoryClassName = (category) => {
-    switch (category) {
-      case "Market":
-        return "market";
-      case "Application":
-        return "application";
-      case "AI":
-        return "ai";
-      case "UI/UX":
-        return "ui-ux";
-      case "Explore":
-        return "explore";
-      case "Figma":
-        return "figma";
-      default:
-        return "";
-    }
-  };
+
   return (
     <>
       <section className="related-blogs-section">
@@ -49,19 +49,20 @@ const RelatedBlogs = ({ relatedBlogs }) => {
                 <span className="date">{relatedBlog.date}</span>
                 <p className="title">{relatedBlog.title}</p>
                 <p className="category">
-                  {Array.isArray(relatedBlog.categories)
-                    ? relatedBlog.categories.map((category, idx) => (
-                        <span
-                          key={`category-${idx}`}
-                          className={getCategoryClassName(category)}
-                        >
-                          {categoryMap[category]}
-                        </span>
-                      ))
-                    : categoryMap[relatedBlog.categories]}
+                  {(Array.isArray(relatedBlog.categories)
+                    ? relatedBlog.categories
+                    : [relatedBlog.categories]
+                  ).map((category, idx) => (
+                    <span
+                      key={`category-${idx}`}
+                      className={getCategoryClassName(category)}
+                    >
+                      {categoryMap[category]}
+                    </span>
+                  ))}
                 </p>
                 <p className="desc-content">
-                  {relatedBlog.description}{" "}
+                  {relatedBlog.description}
                   <span className="read-more">...</span>
                 </p>
                 <Link to={`/blog/${relatedBlog.id}`}>
